2489d058ed80deb1851f5cf49966c44299d1b77c
DeweySR <deweysr@DeweySRs-MacBook-Pro.local>
2019-06-12 12:26:25 -0500

    got a good start at a protoype

--- src/app/services/layout.service.ts
+++ src/app/services/layout.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { GridsterConfig, GridsterItem } from 'angular-gridster2';
+import { GridsterConfig, GridsterItem, GridType, CompactType, DisplayGrid } from 'angular-gridster2';
 import { UUID } from 'angular2-uuid';
 
 export interface IComponent {
@@ -13,37 +13,97 @@ export interface IComponent {
 export class LayoutService {
 
   public options: GridsterConfig = {
+    gridType: GridType.Fit,
+    compactType: CompactType.None,
+    margin: 10,
+    outerMargin: true,
+    outerMarginTop: null,
+    outerMarginRight: null,
+    outerMarginBottom: null,
+    outerMarginLeft: null,
+    useTransformPositioning: true,
+    mobileBreakpoint: 640,
+    minCols: 12,
+    maxCols: 12,
+    minRows: 6,
+    maxRows: 6,
+    maxItemCols: 100,
+    minItemCols: 1,
+    maxItemRows: 100,
+    minItemRows: 1,
+    maxItemArea: 2500,
+    minItemArea: 1,
+    defaultItemCols: 1,
+    defaultItemRows: 1,
+    fixedColWidth: 105,
+    fixedRowHeight: 105,
+    keepFixedHeightInMobile: false,
+    keepFixedWidthInMobile: false,
+    scrollSensitivity: 10,
+    scrollSpeed: 20,
+    enableEmptyCellClick: false,
+    enableEmptyCellContextMenu: false,
+    enableEmptyCellDrop: false,
+    enableEmptyCellDrag: false,
+    emptyCellDragMaxCols: 50,
+    emptyCellDragMaxRows: 50,
+    ignoreMarginInRow: false,
     draggable: {
-      enabled: true
+      enabled: true,
     },
-    pushItems: true,
     resizable: {
-      enabled: true
-    }
+      enabled: true,
+    },
+    swap: false,
+    pushItems: true,
+    disablePushOnDrag: false,
+    disablePushOnResize: false,
+    pushDirections: {north: true, east: true, south: true, west: true},
+    pushResizeItems: false,
+    displayGrid: DisplayGrid.Always,
+    disableWindowResize: false,
+    disableWarnings: false,
+    scrollToNewItems: false
   };
 
   public layout: GridsterItem[] = [];
+  public layout2: GridsterItem[] = [];
   public components: IComponent[] = [];
 
   dropId: string;
 
   constructor() { }
 
-  addItem(): void {
+  addDeliveryToSchedule(cols: number, rows: number, content: string): void {
     this.layout.push({
-      cols: 5,
+      cols: cols,
       id: UUID.UUID(),
-      rows: 5,
+      rows: rows,
       x: 0,
-      y: 0
+      y: 0,
+      resizeEnabled: false,
+      content: content,
+      hasContent: true
     });
   }
 
-  deleteItem(id: string): void {
-    const item = this.layout.find(d => d.id === id);
+  updateResource(modifier: number) {
+    this.options.minRows += modifier;
+    this.options.maxRows += modifier;
+    this.options.api.optionsChanged();
+  }
+
+  // deleteItem(id: string): void {
+  //   const item = this.layout.find(d => d.id === id);
+  //   this.layout.splice(this.layout.indexOf(item), 1);
+  //   const comp = this.components.find(c => c.id === id);
+  //   this.components.splice(this.components.indexOf(comp), 1);
+  // }
+
+  removeItem(item) {
+    // $event.preventDefault();
+    // $event.stopPropagation();
     this.layout.splice(this.layout.indexOf(item), 1);
-    const comp = this.components.find(c => c.id === id);
-    this.components.splice(this.components.indexOf(comp), 1);
   }
 
   setDropId(dropId: string): void {
